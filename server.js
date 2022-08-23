00e3d104b8a9e55d050efc38bf83bb931ab14ebd
Andrea Jared <adj0803@gmail.com>
2022-08-23 04:32:00 -0400

    js and routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = ('./routes/htmlRoutes');
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log('API server now on port${PORT}!');
+});
